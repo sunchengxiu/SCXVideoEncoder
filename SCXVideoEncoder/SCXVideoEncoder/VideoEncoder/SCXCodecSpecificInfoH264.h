ac024dedcc2482562c0a7dd90bdf1d5ef4d880a5
孙承秀 <>
2019-11-21 16:29:58 +0800

    add specificInfoH264 file

---
+++ SCXVideoEncoder/SCXVideoEncoder/VideoEncoder/SCXCodecSpecificInfoH264.h
@@ -0,0 +1,26 @@
+//
+//  SCXCodecSpecificInfoH264.h
+//  SCXVideoEncoder
+//
+//  Created by 孙承秀 on 2019/11/21.
+//  Copyright © 2019 RongCloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+/** Class for H264 specific config. */
+typedef NS_ENUM(NSUInteger, SCXH264PacketizationMode) {
+  SCXH264PacketizationModeNonInterleaved = 0,  // Mode 1 - STAP-A, FU-A is allowed
+  SCXH264PacketizationModeSingleNalUnit        // Mode 0 - only single NALU allowed
+};
+@interface SCXCodecSpecificInfoH264 : NSObject
+
+/**
+ mode
+ */
+@property(nonatomic , assign)SCXH264PacketizationMode packetizationMode;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SCXVideoEncoder/SCXVideoEncoder/VideoEncoder/SCXCodecSpecificInfoH264.m
@@ -0,0 +1,13 @@
+//
+//  SCXCodecSpecificInfoH264.m
+//  SCXVideoEncoder
+//
+//  Created by 孙承秀 on 2019/11/21.
+//  Copyright © 2019 RongCloud. All rights reserved.
+//
+
+#import "SCXCodecSpecificInfoH264.h"
+
+@implementation SCXCodecSpecificInfoH264
+
+@end
